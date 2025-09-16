bc0bc63a5159ac3ce6e5132c7c7fc13150f432d4
PedroJGC <pedro.juliogc02@gmail.com>
2025-09-15 23:07:28 -0300

    initial commit: lesson-01

---
+++ aula-01/index.py
@@ -0,0 +1,23 @@
+print("Questão 1")
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+profissao = input("Digite a sua profissão: ")
+
+print(f"Olá {nome}, você tem {idade} anos e trabalha como {profissao}.")
+
+print("")
+print("Questão 2")
+
+num1 = 12
+num2 = 6
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"A soma dos números é: {soma}")
+print(f"A subtração dos números é: {subtracao}")
+print(f"A multiplicação dos números é: {multiplicacao}")
+print(f"A divisão dos números é: {divisao}")
